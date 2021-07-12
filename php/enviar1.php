c6d8914effca00f4e1af7d1cc7ef7a64bfa7e3e2
Christ0x-ICyS <86194167+Christ0x-ICyS@users.noreply.github.com>
2021-07-12 12:22:36 -0500

    Add files via upload

---
+++ php/enviar1.php
@@ -0,0 +1,21 @@
+<?php
+
+$mail = $_POST['email'];
+
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'raaa@yopmail.com';
+$asunto = 'LIBRO DE RECLAMACIONES';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:reclamaciones.html");
+?>
